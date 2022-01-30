82858b435132ed7345c7a70e804fcb677242df79
Sᴜᴍɪᴛ Yᴀᴅᴀᴠ <91660827+Sumit9969@users.noreply.github.com>
2022-01-30 19:02:18 +0530

    Update user_id.py

--- MusicAndVideo/user_id.py
+++ MusicAndVideo/user_id.py
@@ -15,7 +15,7 @@ async def showid(_, message: Message):
 
     elif chat_type in ["group", "supergroup"]:
         _id = ""
-        _id += "<b>ID group</b>: " f"<code>{message.chat.id}</code>\n"
+        _id += "<b>ɪᴅ ɢʀᴏᴜᴘ</b>: " f"<code>{message.chat.id}</code>\n"
         if message.reply_to_message:
             _id += (
                 "<b>ID pengguna yang dibalas</b>: "
@@ -23,7 +23,7 @@ async def showid(_, message: Message):
             )
             file_info = get_file_id(message.reply_to_message)
         else:
-            _id += "<b>ID pengguna</b>: " f"<code>{message.from_user.id}</code>\n"
+            _id += "<b>ɪᴅ ᴜsᴇʀ</b>: " f"<code>{message.from_user.id}</code>\n"
             file_info = get_file_id(message)
         if file_info:
             _id += (
